1009a3eb608db94251da7895c47fa19d40f5a979
Wei Feng <mathcoder23@126.com>
2022-04-17 13:11:00 +0800

    Update Youtube_SupLoop_Tools.js

--- youtube_SupLoop/Youtube_SupLoop_Tools.js
+++ youtube_SupLoop/Youtube_SupLoop_Tools.js
@@ -7,58 +7,194 @@
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
-// @require      http://code.jquery.com/jquery-2.1.1.min.js
+// @require      https://code.jquery.com/jquery-2.2.4.js
+// @require      https://raw.githubusercontent.com/mathcoder23/Tampermonkey_plugs/main/youtube_SupLoop/framework/vue%402.5.16.js
 // ==/UserScript==
 
-let showPanel = false
-let yuVideo = null
-let setuping = false
-let tagList = [{
-    time: 0
-}]
-let timeout = null
-const setupHtml = () => {
-    if(setuping === true){
-        return
-    }
-    tagList = [{
-        time: 0
-    }]
-    $('#supLoopPanel').remove()
-    setuping = true
-    console.log('set up html', $)
-    let ele = `
-    <div style="display: flex;cursor:pointer;    margin-top: -5px;
-    align-content: center;
-    align-items: center;" id="supLoopButton">
-       <div class='yt-icon-container yt-icon style-scope ytd-button-renderer' ><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"> <path d="M7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z" class="style-scope yt-icon"></path> </g></svg></div>
-       <div id="supLoop" style="font-weight: bold;
-    font-size: small;" class="style-scope ytd-button-renderer style-default size-default" aria-label="Loop">SupLoop</div>
-    </div>
-     `
-    $('#top-level-buttons-computed').append(ele)
+const supLoopPanelVueHtml = `
+      <div id="supLoopPanel" style="display: none;">
+           <div style="padding:10px;font-weight: var(--ytd-tab-system_-_font-weight);color:var(--yt-button-icon-button-text-color,var(--yt-spec-text-secondary));font-size: small;">
 
-    // insert panel
-    let panel = `
-       <div id="supLoopPanel">
-           <div style="padding:10px;font-weight: bold;">
-           Welcome SupLoop!
+           <span>Welcome {{name}}!</span>
+           <span style="margin-left:20px;" >Status:{{status}}</span>
            </div>
-           <div id="supLoopTagList">
 
-           </div>
+            <div id="supLoopTagList" >
+                <div style="display:flex;" >
+                    <div v-for="(item,index) in timePointList" class='loop_tag ' :class="{'loop_active':item.isLoop}"
+                    @click="clickTimePoint(index,item)"
+                    @dblclick="dblclickTimePoint(index,item)"
+                    >{{formatTime(item.time)}}</div>
+                </div>
+            </div>
+
+            <div style="padding:10px;font-weight: var(--ytd-tab-system_-_font-weight);color:var(--yt-button-icon-button-text-color,var(--yt-spec-text-secondary));font-size: small;">
+                <p>Help: </p>
+                <p>keydown: key a is insert point</p>
+                <p>keydown: key g is start loop</p>
+                <p>keydown: key s is stop loop</p>
+                <p>click timePoint is switch loop section</p>
+                <p>double click timePoint is delete timePoint</p>
+            </div>
        </div>
-    `
-    $('#meta').before(panel)
+       <style>
+            .loop_tag{
+                margin-left: 10px;
+                padding: 5px;
+                background: gray;
+                cursor: pointer;
+            }
 
-    $('#supLoop').click(() => {
-        switchPanel()
-    })
-    setuping = false
-}
+            .loop_active{
+                background: #3ccccc;
+            }
+        </style>
+`
 
-const switchPanel = (showPanel) => {
-    let show = $('#supLoopPanel').toggle()
+const supLoopPanelHandler = () => {
+    window.supLoopVueApp = new Vue({
+        el: "#supLoopPanel",
+        data: {
+            name: "SupLoop",
+            timePointList: [
+                {
+                    time: 0,
+                    isLoop: false
+                }
+            ],
+
+            loopTimeout: null,
+            status: 'StopLoop',
+            currentHref: ''
+        },
+        methods: {
+            formatTime(time) {
+                var h = Math.floor(time / 3600);
+                var m = Math.floor((time / 60 % 60));
+                var s = Math.floor((time % 60));
+                if (h < 1) {
+                    return (m < 10 ? ('0' + m) : (m)) + ":" + (s < 10 ? ('0' + s) : (s));
+                } else {
+                    return (h < 10 ? ('0' + h) : (h)) + ":" + (m < 10 ? ('0' + m) : (m)) + ":" + (s < 10 ? ('0' + s) : (s));
+                }
+            },
+            clickTimePoint(index, timePoint) {
+                let video = $('video')[0]
+                video.currentTime = timePoint.time
+
+                if (index < this.timePointList.length - 1) {
+                    for (let item of this.timePointList) {
+                        item.isLoop = false
+                    }
+                    this.timePointList[index].isLoop = true
+                    this.timePointList[index + 1].isLoop = true
+                }
+            },
+            dblclickTimePoint(index, timePoint) {
+                this.stopLoop()
+                this.timePointList.splice(index, 1)
+                this.saveRecord()
+            },
+            onKeydown(e) {
+                console.log('key', e.keyCode)
+
+                if (e.keyCode === 65) {
+                    // key a
+                    this.insertTimePoint()
+                } else if (e.keyCode === 71) {
+                    // key g
+                    this.status = 'Looping'
+                    this.goLoop()
+                } else if (e.keyCode === 83) {
+                    // key s
+                    this.status = 'Stop Loop'
+                    this.stopLoop()
+                }
+            },
+            insertTimePoint() {
+                let v = $('video')[0]
+                for (let i = 0; i < this.timePointList.length - 1; i++) {
+                    this.timePointList[i].isLoop = false
+
+                }
+                this.timePointList.push({
+                    time: v.currentTime,
+                    isLoop: true
+                })
+                this.saveRecord()
+            },
+            goLoop() {
+                this.stopLoop()
+
+                if (this.timePointList.length > 1) {
+                    // search loop index
+                    let startLoop = null
+                    let endLoop = null
+                    for (let i = 0; i < this.timePointList.length; i++) {
+                        if (null === startLoop && this.timePointList[i].isLoop) {
+                            startLoop = this.timePointList[i]
+                        }
+                        let j = this.timePointList.length - i - 1
+                        if (null === endLoop && this.timePointList[j].isLoop) {
+                            endLoop = this.timePointList[j]
+                        }
+                    }
+                    console.log('startLoop', startLoop, 'endLoop', endLoop)
+                    this.startVideo(startLoop.time, endLoop.time)
+                }
+
+            },
+            stopLoop() {
+                if (null !== this.loopTimeout) {
+                    clearTimeout(this.loopTimeout)
+                    this.loopTimeout = null
+                }
+
+            },
+            startVideo(startTime, endTime) {
+                if (this.currentHref !== location.href) {
+                    console.log('close loop:', this.currentHref, location.href)
+                    return
+                }
+                console.log('startVideoLoop', startTime, endTime)
+                let video = $('video')[0]
+                video.currentTime = startTime
+                video.play()
+                this.loopTimeout = setTimeout(() => {
+                    this.startVideo(startTime, endTime)
+                }, (endTime - startTime) * 1000)
+            },
+            saveRecord() {
+                let record = {
+                    showPanel: this.showPanel,
+                    timePointList: this.timePointList
+                }
+                localStorage.setItem('supLoopRecord-' + location.href, JSON.stringify(record))
+            },
+            loadRecord() {
+                let str = localStorage.getItem('supLoopRecord-' + location.href)
+                if (str && str.length > 0) {
+                    try {
+                        this.record = JSON.parse(str)
+                        this.timePointList = this.record.timePointList
+                        this.showPanel = this.record.showPanel
+                    } catch (e) {
+                        console.log('err', e)
+                        localStorage.removeItem('supLoopRecord-' + location.href)
+                    }
+                }
+            }
+        },
+        mounted() {
+            $(document).keydown((e) => {
+                this.onKeydown(e)
+            });
+            this.currentHref = location.href
+            console.log('mounted supLoopPanel')
+            this.stopLoop()
+            this.loadRecord()
+        }
+    });
 }
 
 const keyEvent = (e) => {
@@ -66,7 +202,7 @@ const keyEvent = (e) => {
 
     if (e.keyCode === 65) {
         insertTag()
-        renderTagList()
+        //renderTagList()
     } else if (e.keyCode === 71) {
         goTag()
     } else if (e.keyCode === 83) {
@@ -106,31 +242,68 @@ const stopTag = () => {
     }
 }
 
+
+
+
+
+let showPanel = false
+let yuVideo = null
+let setuping = false
+let tagList = [{
+    time: 0
+}]
+let timeout = null
+const setupHtml = () => {
+    if (setuping === true) {
+        return
+    }
+    tagList = [{
+        time: 0
+    }]
+    $('#supLoopPanel').remove()
+    setuping = true
+    console.log('set up html', $)
+    let ele = `
+    <div style="display: flex;cursor:pointer;    margin-top: -5px;
+    align-content: center;
+    align-items: center;" id="supLoopButton">
+       <div style="width: 24px;height: 24px;" class='yt-icon-container yt-icon style-scope ytd-button-renderer' ><svg style="width: 100%;height: 100%;" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"> <path d="M7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z" class="style-scope yt-icon"></path> </g></svg></div>
+       <div id="supLoop" style="font-weight: var(--ytd-tab-system_-_font-weight);color:var(--yt-button-icon-button-text-color,var(--yt-spec-text-secondary));
+    font-size: small;" class="style-scope ytd-button-renderer style-default size-default" aria-label="Loop">SupLoop</div>
+    </div>
+     `
+    $('#top-level-buttons-computed').append(ele)
+
+    // insert panel
+
+    $('#meta').before(supLoopPanelVueHtml)
+
+    $('#supLoop').click(() => {
+        switchPanel()
+    })
+    supLoopPanelHandler()
+
+    setuping = false
+}
+
+const switchPanel = (showPanel) => {
+    let show = $('#supLoopPanel').toggle()
+}
+
+
+
 const renderTagList = () => {
     let tagListHtml = '<div style="display:flex;">'
     let i = 0
     for (let tag of tagList) {
         let tagId = `supLoopTag${i}`
-        tagListHtml += `<div class='loop_tag ${tag.isLoop?'loop_active':''}' id="${tagId}" >${formatTime(tag.time)}</div>`
+        tagListHtml += `<div class='loop_tag ${tag.isLoop ? 'loop_active' : ''}' id="${tagId}" >${formatTime(tag.time)}</div>`
 
         i++
     }
     tagListHtml += `
     </div>
-    <style>
-        .loop_tag{
-    margin-left: 10px;
-    padding: 5px;
-    background: gray;
-    cursor: pointer;
-        }
 
-        .loop_active{
-        background: #3ccccc;
-        }
-
-
-    </style>
     `
     $('#supLoopTagList').html(tagListHtml)
 
@@ -185,22 +358,20 @@ const startVideo = (startTime, endTime) => {
     }, (endTime - startTime) * 1000)
 }
 
-const checkSetup = ()=>{
+const checkSetup = () => {
 
-    $(document).keydown(function (e) {
-        keyEvent(e)
-    });
 
-    setInterval(()=>{
-        if(!$('#supLoopButton')[0]){
+
+    setInterval(() => {
+        if (!$('#supLoopButton')[0]) {
             setupHtml()
         }
-    },1000)
+    }, 1000)
 }
 
 (function () {
     'use strict';
+
     checkSetup()
 
-    // Your code here...
 })();
