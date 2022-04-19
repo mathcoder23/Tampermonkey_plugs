3ce88b5f64b77dd03357a88817156e500e17fb90
Wei Feng <mathcoder23@126.com>
2022-04-19 09:08:17 +0800

    Update Youtube_SupLoop_Tools.js

--- youtube_SupLoop/Youtube_SupLoop_Tools.js
+++ youtube_SupLoop/Youtube_SupLoop_Tools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube SupLoop Tools
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/*
